b4e6205e1744fa7c3f53c4ba5923bf8634232153
Kaique Geraldo <kaique.candido@aluno.senai.br>
2025-05-16 03:44:51 -0300

    ajuste geral frontend

--- projetos-to-do-frontend/src/app/(public)/aprovar-vinculo/page.tsx
+++ projetos-to-do-frontend/src/app/(public)/aprovar-vinculo/page.tsx
@@ -1,9 +1,15 @@
+import { Suspense } from "react";
 import AprovarVinculoScreen from "@/screens/(aprovar-vinculo)/aprovar-vinculo";
 
 export const metadata = {
   title: "MindTask - Aprovar Vinculo",
 };
 
-export default function AprovarPage() {
-  return <AprovarVinculoScreen />;
+
+export default function AprovarVinculoPage() {
+  return (
+    <Suspense fallback={<div>Carregando...</div>}>
+      <AprovarVinculoScreen />
+    </Suspense>
+  );
 }
